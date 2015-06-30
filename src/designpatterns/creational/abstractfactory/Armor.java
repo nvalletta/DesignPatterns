1116194a1d0427d98f8e62efffc0b04924e9282a
Nora Valletta <nora.valletta@gmail.com>
2015-06-30 10:34:13 -0600

    Adds basic models

---
+++ src/designpatterns/creational/abstractfactory/Armor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.creational.abstractfactory;
+
+/**
+ *
+ * @author nora-valletta
+ */
+public abstract class Armor {
+    private Gem gem;
+    private Enchant enchant;
+    private Material material;
+}
---
+++ src/designpatterns/creational/abstractfactory/Weapon.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.creational.abstractfactory;
+
+/**
+ *
+ * @author nora-valletta
+ */
+public class Weapon {
+    private Material material;
+    private Gem gem;
+    private Enchant enchant;
+}
